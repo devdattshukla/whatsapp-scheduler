656fa1d9e23f6f8efa442db8559fdd34444308cc
devdattshukla <85863045+devdattshukla@users.noreply.github.com>
2026-06-10 10:42:06 +0530

    Update send.py

--- send.py
+++ send.py
@@ -1,4 +1,6 @@
-response = requests.post(url, headers=headers, json=data)
-
-print("STATUS CODE:", response.status_code)
-print("RESPONSE:", response.text)
+Run python send.py
+Traceback (most recent call last):
+  File "/home/runner/work/whatsapp-scheduler/whatsapp-scheduler/send.py", line 1, in <module>
+    response = requests.post(url, headers=headers, json=data)
+NameError: name 'requests' is not defined
+Error: Process completed with exit code 1.
