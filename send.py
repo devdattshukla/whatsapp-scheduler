b44c9b5c946b77092bf81cd271c5d1e32eb3763d
devdattshukla <85863045+devdattshukla@users.noreply.github.com>
2026-06-10 10:48:30 +0530

    Update send.py

--- send.py
+++ send.py
@@ -1,6 +1,28 @@
-Run python send.py
-Traceback (most recent call last):
-  File "/home/runner/work/whatsapp-scheduler/whatsapp-scheduler/send.py", line 1, in <module>
-    response = requests.post(url, headers=headers, json=data)
-NameError: name 'requests' is not defined
-Error: Process completed with exit code 1.
+import os
+import requests
+
+TOKEN = os.environ["TOKEN"]
+PHONE_NUMBER_ID = os.environ["PHONE_NUMBER_ID"]
+
+# 👉 YOUR WhatsApp number (test recipient)
+TO_NUMBER = "919925210608"   # replace this
+
+url = f"https://graph.facebook.com/v20.0/{PHONE_NUMBER_ID}/messages"
+
+headers = {
+    "Authorization": f"Bearer {TOKEN}",
+    "Content-Type": "application/json"
+}
+
+data = {
+    "messaging_product": "whatsapp",
+    "to": TO_NUMBER,
+    "type": "text",
+    "text": {
+        "body": "🎉 GitHub Actions test message successful!"
+    }
+}
+
+response = requests.post(url, headers=headers, json=data)
+print(response.status_code)
+print(response.text)
