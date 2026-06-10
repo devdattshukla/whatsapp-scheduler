435033360a4fdbf204d5994cfba0c555401a62bc
devdattshukla <85863045+devdattshukla@users.noreply.github.com>
2026-06-10 21:43:56 +0530

    Add debug prints for birthday checks and responses

--- send.py
+++ send.py
@@ -8,13 +8,20 @@ PHONE_NUMBER_ID = os.environ["PHONE_NUMBER_ID"]
 
 today = datetime.now().strftime("%m-%d")
 
+print("Today:", today)
+
 with open("contacts.json", "r") as f:
     contacts = json.load(f)
 
 for contact in contacts:
-    if contact["birthday"] == today:
 
-        url = f"https://graph.facebook.com/v20.0/{PHONE_NUMBER_ID}/messages"
+    print("Checking:", contact["name"])
+    print("Birthday:", contact["birthday"])
+
+    # CHANGE TO True FOR TESTING
+    if True:
+
+        url = f"https://graph.facebook.com/v23.0/{PHONE_NUMBER_ID}/messages"
 
         headers = {
             "Authorization": f"Bearer {TOKEN}",
@@ -32,5 +39,5 @@ for contact in contacts:
 
         response = requests.post(url, headers=headers, json=data)
 
-print(response.status_code)
-print(response.text)
+        print("STATUS CODE:", response.status_code)
+        print("RESPONSE:", response.text)
